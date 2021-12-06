b518cd3561b81f9ac959d4c0b6761ec544d41ae0
Sam Ferrell <mail@samcf.me>
2021-12-06 08:50:59 -0700

    Simplify Clojure solution for day one.

--- sonar-sweep.clj
+++ sonar-sweep.clj
@@ -1,21 +1,16 @@
 (ns advent)
 
+(defn a [coll]
+  (->> (partition 2 1 coll)
+       (reduce (fn [t [a b]] (+ t (if (< a b) 1 0))) 0)))
+
+(defn b [coll]
+  (->> (partition 3 1 coll)
+       (partition 2 1)
+       (reduce (fn [t [a b]] (+ t (if (< (reduce + a) (reduce + b)) 1 0))) 0)))
+
 (def depths
   (into [] (map #(Integer. %)) (line-seq (java.io.BufferedReader. *in*))))
 
-(defn a []
-  (->> (partition 2 1 depths)
-       (map (partial apply <))
-       (filter identity)
-       (count)))
-
-(defn b []
-  (->> (partition 3 1 depths)
-       (map (partial apply +))
-       (partition 2 1)
-       (map (partial apply <))
-       (filter identity)
-       (count)))
-
-(println (a))
-(println (b))
+(println (a depths))
+(println (b depths))
