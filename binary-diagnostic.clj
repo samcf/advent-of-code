093ce1a81f0baf33496df15c660ab63e7c08715f
Sam Ferrell <mail@samcf.me>
2021-12-08 07:42:19 -0700

    Simplify Clojure day three part one solution.

--- binary-diagnostic.clj
+++ binary-diagnostic.clj
@@ -1,20 +1,15 @@
 (ns advent)
 
 (defn a [coll]
-  (let [[digits _] (first coll)
-        mask       (bit-shift-left 1 digits)
-        gamma      (->> (reverse (range 1 (+ digits 1)))
-                        (map (fn [n] (bit-shift-left 1 (- n 1))))
-                        (reduce
-                         (fn [g m]
-                           (if (> (reduce (fn [c [_ n]] (if (= (bit-and m n) 0) c (inc c))) 0 coll) (/ (count coll) 2))
-                             (bit-or g m) g)) 0))]
-    (* gamma (bit-and (bit-not gamma) (- mask 1)))))
-
-(defn b [coll] 0)
+  (->> (apply mapv vector coll)
+       (map (fn [x] (reduce (fn [t n] (if (= n \1) (inc t) t)) 0 x)))
+       (map (fn [x] (if (> x (/ (count coll) 2)) [1 0] [0 1])))
+       (apply mapv vector)
+       (map (fn [x] (apply str x)))
+       (map (fn [x] (Integer/parseInt x 2)))
+       (apply *)))
 
 (def reports
-  (into [] (map (juxt count #(Integer/parseInt % 2))) (line-seq (java.io.BufferedReader. *in*))))
+  (into [] (line-seq (java.io.BufferedReader. *in*))))
 
 (println "Part A:" (a reports))
-(println "Part B:" (b reports))
