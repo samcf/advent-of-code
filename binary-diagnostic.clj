4c15196194f2e9b867e4d5e250332a3ee323e8ca
Sam Ferrell <mail@samcf.me>
2021-12-08 08:51:36 -0700

    Clojure solution for day three part two.

--- binary-diagnostic.clj
+++ binary-diagnostic.clj
@@ -9,7 +9,19 @@
        (map (fn [x] (Integer/parseInt x 2)))
        (apply *)))
 
+(defn f [xs idx hi lo]
+  (if (seq (rest xs))
+    (let [col (map (fn [x] (nth x idx)) xs)
+          sum (reduce (fn [t x] (if (= x \1) (inc t) t)) 0 col)
+          res (if (>= sum (/ (count xs) 2)) hi lo)]
+      (recur (filter (fn [x] (= (get x idx) res)) xs) (inc idx) hi lo))
+    (Integer/parseInt (apply str (first xs)) 2)))
+
+(defn b [coll]
+  (* (f coll 0 \1 \0) (f coll 0 \0 \1)))
+
 (def reports
   (into [] (line-seq (java.io.BufferedReader. *in*))))
 
 (println "Part A:" (a reports))
+(println "Part B:" (b reports))
