6c75a1da26ae22fb732c2a4ea737626c26a25c4c
Sam Ferrell <mail@samcf.me>
2021-12-11 13:08:46 -0700

    Additional refactor of day four for Clojure.

--- day-04-giant-squid.clj
+++ day-04-giant-squid.clj
@@ -4,22 +4,15 @@
 
 (def masks [0x1F00000 0xF8000 0x7C00 0x3E0 0x1F 0x1084210 0x842108 0x421084 0x210842 0x108421])
 
-(defn scores
-  "Returns a sorted map whose keys are scores and whose values are the sum of
-   unmarked numbers multiplied by the number that was called when the board
-   was completed."
-  [boards xs]
-  (into (sorted-map)
-        (map (fn [board]
-               (let [vs (set (keys board))]
-                 (loop [prev 0 [x & xs] xs]
-                   (if-let [index (board x)]
-                     (let [next (bit-or prev (bit-shift-left 1 (- 24 index)))]
-                       (if (some #(= (bit-and next %) %) masks)
-                         [(+ (count xs) 1)
-                          (->> (intersection (set xs) vs) (reduce +) (* x))]
-                         (recur next xs)))
-                     (recur prev xs)))))) boards))
+(defn play [xs board]
+  (loop [prev 0 [x & xs] xs]
+    (if-let [index (board x)]
+      (let [next (bit-or prev (bit-shift-left 1 (- 24 index)))]
+        (if (some #(= (bit-and next %) %) masks) [(keys board) xs x] (recur next xs)))
+      (recur prev xs))))
+
+(defn score-kv [[ks xs x]]
+  [(count xs) (* (reduce + (intersection (set xs) (set ks))) x)])
 
 (def parse-xf
   (comp (filter seq)
@@ -31,10 +24,10 @@
         (map (fn [xs] (map-indexed (comp vec rseq vector) xs)))
         (map (partial into {}))))
 
-(def input
-  (let [[xs & bs] (into [] (line-seq (java.io.BufferedReader. *in*)))]
-    [(into [] parse-xf bs) (map #(Integer. %) (split xs #","))]))
-
-(let [scored (apply scores input)]
-  (println "Part A:" (val (last scored)))
-  (println "Part B:" (val (first scored))))
+(let [[xs & rest] (into [] (line-seq (java.io.BufferedReader. *in*)))
+      boards      (into [] parse-xf rest)
+      digits      (map #(Integer. %) (split xs #","))
+      scores      (into (sorted-map) (comp (map (partial play digits)) (map score-kv)) boards)
+      values      (into [] (map val) scores)]
+  (println "Part A:" (peek values))
+  (println "Part B:" (first values)))
