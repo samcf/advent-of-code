9d5a63fb7edca95b183bdeb82b6fb53bb4f3a27f
Sam Ferrell <mail@samcf.me>
2021-12-14 14:04:59 -0700

    Solution for both parts of day six in Clojure.

---
+++ day-06-lanternfish.clj
@@ -0,0 +1,15 @@
+(ns advent
+  (:require [clojure.string :refer [split]]))
+
+(defn inc- [x] (+ (or x 0) 1))
+
+(defn fish [init days]
+  (->> (range 7 (+ days 1))
+       (reduce (fn [m d] (assoc m d (+ (get m (- d 7) 0) (get m (- d 9) 0)))) init)
+       (reduce (fn [t [_ v]] (+ t v)) 0)))
+
+(let [line (first (line-seq (java.io.BufferedReader. *in*)))
+      nums (into [] (map (fn [x] (Integer. x))) (split line #","))
+      init (reduce (fn [m x] (update m (inc x) inc-)) {} nums)]
+  (println "Part A:" (+ (fish init 80) (count nums)))
+  (println "Part B:" (+ (fish init 256) (count nums))))
