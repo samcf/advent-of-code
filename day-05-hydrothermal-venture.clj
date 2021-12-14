fcb75ede97a38a5c03d2ad39d5707a7f18a8e0c3
Sam Ferrell <mail@samcf.me>
2021-12-14 04:42:32 -0700

    Solution to both parts of day five in Clojure.

---
+++ day-05-hydrothermal-venture.clj
@@ -0,0 +1,23 @@
+(ns advent
+  (:require [clojure.string :refer [split]]))
+
+(defn spread [src dst]
+  (if (> src dst) (range src (- dst 1) -1) (range src (+ dst 1))))
+
+(defn points [[ax ay bx by]]
+  (cond (= ax bx) (map (fn [v] [ax v]) (spread ay by))
+        (= ay by) (map (fn [v] [v ay]) (spread ax bx))
+        :else (apply mapv vector [(spread ax bx) (spread ay by)])))
+
+(defn counts [coll]
+  (->> (into [] (comp (map points) cat) coll)
+       (frequencies)
+       (reduce (fn [t [_ v]] (if (> v 1) (inc t) t)) 0)))
+
+(def vecs-xf (comp (map (fn [xs] (split xs #","))) cat (map (fn [x] (Integer. x)))))
+(def line-xf (comp (map (fn [ls] (split ls #" -> "))) (map (fn [xs] (into [] vecs-xf xs)))))
+
+(let [lines (line-seq (java.io.BufferedReader. *in*))
+      parse (into [] line-xf lines)]
+  (println "Part A:" (counts (filter (fn [[ax ay bx by]] (or (= ax bx) (= ay by))) parse)))
+  (println "Part B:" (counts parse)))
