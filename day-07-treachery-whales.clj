cb288cd048d89612b65611f976682def9a078cb7
Sam Ferrell <mail@samcf.me>
2021-12-15 14:50:04 -0700

    Minor cleanup.

--- day-07-treachery-whales.clj
+++ day-07-treachery-whales.clj
@@ -4,14 +4,14 @@
 (defn cost-a [a b] (Math/abs (- a b)))
 (defn solve-a [xs]
   (let [xs (into [] (sort xs)) m (xs (/ (count xs) 2))]
-    (transduce (map (fn [x] (cost-a x m))) + 0 xs)))
+    (transduce (map (partial cost-a m)) + 0 xs)))
 
 (defn cost-b [a b] (let [n (Math/abs (- a b))] (int (* (/ n 2) (+ n 1)))))
 (defn solve-b [xs]
   (let [m (int (/ (reduce + xs) (count xs)))]
-    (transduce (map (fn [x] (cost-b x m))) + 0 xs)))
+    (transduce (map (partial cost-b m)) + 0 xs)))
 
-(let [line (first (line-seq (java.io.BufferedReader. *in*)))
-      nums (into [] (map (fn [x] (Integer. x)) (split line #",")))]
-  (println "Part A:" (solve-a nums))
-  (println "Part B:" (solve-b nums)))
+(let [ln (first (line-seq (java.io.BufferedReader. *in*)))
+      xs (into [] (map (fn [x] (Integer. x))) (split ln #","))]
+  (println "Part A:" (solve-a xs))
+  (println "Part B:" (solve-b xs)))
