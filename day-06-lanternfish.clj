ffed6f794ba239326939d8e35f28aca2c34c5807
Sam Ferrell <mail@samcf.me>
2021-12-15 18:18:36 -0700

    Clean up day six.

--- day-06-lanternfish.clj
+++ day-06-lanternfish.clj
@@ -1,15 +1,14 @@
 (ns advent
   (:require [clojure.string :refer [split]]))
 
-(defn inc- [x] (+ (or x 0) 1))
+(defn solve [xs days]
+  (->> (range 7 (inc days))
+       (reduce (fn [m x] (assoc m x (+ (get m (- x 7) 0) (get m (- x 9) 0))))
+               (frequencies xs))
+       (transduce (map val) + 0)
+       (+ (count xs))))
 
-(defn fish [init days]
-  (->> (range 7 (+ days 1))
-       (reduce (fn [m d] (assoc m d (+ (get m (- d 7) 0) (get m (- d 9) 0)))) init)
-       (transduce (map val) + 0)))
-
-(let [line (first (line-seq (java.io.BufferedReader. *in*)))
-      nums (into [] (map (fn [x] (Integer. x))) (split line #","))
-      init (reduce (fn [m x] (update m (inc x) inc-)) {} nums)]
-  (println "Part A:" (+ (fish init 80) (count nums)))
-  (println "Part B:" (+ (fish init 256) (count nums))))
+(let [ln (first (line-seq (java.io.BufferedReader. *in*)))
+      xs (into [] (comp (map #(Integer. %)) (map inc)) (split ln #","))]
+  (println "Part A:" (solve xs 80))
+  (println "Part B:" (solve xs 256)))
