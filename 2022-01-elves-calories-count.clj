0eec492db43f7a58368c38a08b1265a2b7d1b89c
Sam Ferrell <mail@samcf.me>
2022-12-01 18:51:03 -0700

    Solution for Day 1

---
+++ 2022-01-elves-calories-count.clj
@@ -0,0 +1,10 @@
+(ns advent)
+
+(let [lns (line-seq (java.io.BufferedReader. *in*))
+      sum (into (sorted-set-by >)
+                (comp (map #(if (= % "") 0 (Integer. %)))
+                      (partition-by zero?)
+                      (map #(apply + %)))
+                lns)]
+  (println "Part A:" (first sum))
+  (println "Part B:" (apply + (take 3 sum))))
