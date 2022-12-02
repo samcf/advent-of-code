3dcbb0e8be4786a700f40c71e947e7baad0c8c24
Sam Ferrell <mail@samcf.me>
2022-12-01 22:46:59 -0700

    Simplify solution for Day 2

--- 2022-02-rock-paper-scissors.clj
+++ 2022-02-rock-paper-scissors.clj
@@ -1,10 +1,9 @@
 (ns advent
   (:require [clojure.string]))
 
-(def solve-a {["A" "X"] 4 ["B" "X"] 1 ["C" "X"] 7 ["A" "Y"] 8 ["B" "Y"] 5 ["C" "Y"] 2 ["A" "Z"] 3 ["B" "Z"] 9 ["C" "Z"] 6})
-(def solve-b {["A" "X"] 3 ["B" "X"] 1 ["C" "X"] 2 ["A" "Y"] 4 ["B" "Y"] 5 ["C" "Y"] 6 ["A" "Z"] 8 ["B" "Z"] 9 ["C" "Z"] 7})
+(def solve-a {"A X" 4 "B X" 1 "C X" 7 "A Y" 8 "B Y" 5 "C Y" 2 "A Z" 3 "B Z" 9 "C Z" 6})
+(def solve-b {"A X" 3 "B X" 1 "C X" 2 "A Y" 4 "B Y" 5 "C Y" 6 "A Z" 8 "B Z" 9 "C Z" 7})
 
-(let [lns (line-seq (java.io.BufferedReader. *in*))
-      prs (map (fn [ln] (clojure.string/split ln #" ")) lns)]
-  (println "Part A:" (transduce (map solve-a) + prs))
-  (println "Part B:" (transduce (map solve-b) + prs)))
+(let [lns (line-seq (java.io.BufferedReader. *in*))]
+  (println "Part A:" (transduce (map solve-a) + lns))
+  (println "Part B:" (transduce (map solve-b) + lns)))
