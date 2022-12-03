0e0896d848bf175a4f9f2fd934e2060d0a1d5187
Sam Ferrell <mail@samcf.me>
2022-12-03 02:20:50 -0700

    Improved Day 3 solution

--- 2022-03-rucksack-reorg.clj
+++ 2022-03-rucksack-reorg.clj
@@ -1,10 +1,10 @@
 (ns advent
   (:require [clojure.set :refer [intersection]]))
 
-(defn priority [c]
-  (if (Character/isUpperCase c)
-    (+ (- (int c) (int \A)) 27)
-    (+ (- (int c) (int \a)) 1)))
+(def priority
+  (->> (range 1 53)
+       (interleave "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+       (apply hash-map)))
 
 (def score-xf
   (comp (map (partial map set))
@@ -13,5 +13,5 @@
         (map priority)))
 
 (let [lns (line-seq (java.io.BufferedReader. *in*))]
-  (println "Part A:" (transduce (comp (map #(partition (/ (count %) 2) %)) score-xf) + lns))
+  (println "Part A:" (transduce (comp (map #(split-at (/ (count %) 2) %)) score-xf) + lns))
   (println "Part B:" (transduce (comp (partition-all 3) score-xf) + lns)))
