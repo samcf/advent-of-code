8a79789bc371fa5f91d7920b9bae5de0eca02487
Sam Ferrell <mail@samcf.me>
2022-12-02 23:39:02 -0700

    Addtl cleanup for Day 3 2022

--- 2022-03-rucksack-reorg.clj
+++ 2022-03-rucksack-reorg.clj
@@ -1,16 +1,17 @@
 (ns advent
   (:require [clojure.set :refer [intersection]]))
 
-(def priority
-  (into {} (comp (partition-all 2) (map (fn [[c i]] [(char c) i])))
-        (interleave (concat (range 65 91) (range 97 124)) (range 1 53))))
+(defn priority [c]
+  (if (Character/isUpperCase c)
+    (+ (- (int c) (int \A)) 27)
+    (+ (- (int c) (int \a)) 1)))
 
-(def sum-xf
+(def score-xf
   (comp (map (partial map set))
         (map (partial apply intersection))
         (map first)
         (map priority)))
 
 (let [lns (line-seq (java.io.BufferedReader. *in*))]
-  (println "Part A:" (transduce (comp (map #(partition (/ (count %) 2) %)) sum-xf) + lns))
-  (println "Part B:" (transduce (comp (partition-all 3) sum-xf) + lns)))
+  (println "Part A:" (transduce (comp (map #(partition (/ (count %) 2) %)) score-xf) + lns))
+  (println "Part B:" (transduce (comp (partition-all 3) score-xf) + lns)))
