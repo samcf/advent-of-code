21d935b9b129ee9887c8cf35a2f42b9121a7268b
Sam Ferrell <mail@samcf.me>
2022-12-07 16:29:29 -0700

    Improvement to Day 7 solution.

--- 2022-07-no-space-left.clj
+++ 2022-07-no-space-left.clj
@@ -1,14 +1,14 @@
+(defn walk-and-sum [[tree path] ln]
+  (condp re-find ln
+    #"\$ cd /"     [tree [["/"]]]
+    #"\$ cd \.\."  [tree (pop path)]
+    #"\$ cd (\S+)" :>> (fn [[_ dir]] [tree (conj path (conj (peek path) dir))])
+    #"(\d+)"       :>> (fn [[_ siz]] [(reduce (fn [tree dir] (update tree dir #(+ (or %1 0) %2) (Integer. siz))) tree path) path])
+    [tree path]))
+
 (let [lns      (line-seq (java.io.BufferedReader. *in*))
-      [tree _] (reduce (fn [[tree path] ln]
-                         (condp re-find ln
-                           #"\$ cd /" :>>     (fn [_] [tree ["/"]])
-                           #"\$ cd \.\." :>>  (fn [_] [tree (pop path)])
-                           #"\$ cd (\S+)" :>> (fn [[_ dir]] [tree (conj path (conj path dir))])
-                           #"(\d+) (\S+)" :>> (fn [[_ size _]] [(reduce (fn [tree dir] (update tree dir #(+ (or %1 0) %2) (Integer. size))) tree path) path])
-                           [tree path]))
-                       [{"/" 0} []] lns)]
+      [tree _] (reduce walk-and-sum [{["/"] 0} []] lns)]
   (println "Part A:" (transduce (comp (filter #(<= (second %) 100000)) (map val)) + tree))
-  (println "Part B:" (->> tree
-                          (filter (fn [[_ size]] (->> (tree "/") (- 70000000) (- 30000000) (>= size))))
+  (println "Part B:" (->> (filter (fn [[_ size]] (->> (tree ["/"]) (- 70000000) (- 30000000) (>= size))) tree)
                           (apply min-key second)
-                          val)))
+                          (val))))
