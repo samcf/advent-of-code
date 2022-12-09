b54529c757da1206f795ae2bb253cc00a3806363
Sam Ferrell <mail@samcf.me>
2022-12-09 06:40:29 -0700

    Improvements for Day 9 solution.

--- 2022-09-rope-bridge.clj
+++ 2022-09-rope-bridge.clj
@@ -13,14 +13,20 @@
 
 (let [lines (line-seq (java.io.BufferedReader. *in*))
       moves (sequence (comp (map parse-ln) cat) lines)
-      solve (fn [n] (reduce (fn [[vis crd] [ox oy]]
-                              (let [[ax ay] (first crd)
-                                    [dx dy] [(+ ax ox) (+ ay oy)]]
-                                [(conj vis (last crd))
-                                 (reduce (fn [crd [idx vct]] (assoc crd (inc idx) (follow vct (crd idx))))
-                                         (assoc crd 0 [dx dy])
-                                         (map-indexed vector (rest crd)))]))
-                            [#{[0 0]} (vec (repeat n [0 0]))]
-                            moves))]
-  (println "Part A:" (count (first (solve 2))))
-  (println "Part B:" (count (first (solve 10)))))
+      solve #(loop [knots   (vec (repeat % [0 0]))
+                    offsets moves
+                    visited #{}]
+               (if (seq offsets)
+                 (let [[ax ay] (first knots)
+                       [ox oy] (first offsets)]
+                   (recur (loop [knots (assoc knots 0 [(+ ax ox) (+ ay oy)])
+                                 idx   1]
+                            (if (< idx (count knots))
+                              (recur (update knots idx follow (knots (dec idx)))
+                                     (inc idx))
+                              knots))
+                          (rest offsets)
+                          (conj visited (last knots))))
+                 (count visited)))]
+  (println "Part A:" (solve 2))
+  (println "Part B:" (solve 10)))
