8411d2d4a47382c29352199d5eb12670ef568b1e
Sam Ferrell <mail@samcf.me>
2022-12-14 04:59:29 -0700

    Improve Day 10 solution.

--- 2022-10-cathode-raytube.clj
+++ 2022-10-cathode-raytube.clj
@@ -1,25 +1,21 @@
 (require '[clojure.string :refer [join]])
 
-(defn parse-ln [ln]
-  (condp re-find ln
-    #"noop"             [1 0]
-    #"addx (-?\d+)" :>> (fn [[_ x]] [2 (Integer. x)])))
-
-(def xf-render
+(def render
   (comp (map-indexed vector)
         (map (fn [[idx val]]
-               (if (<= val idx (+ val 2)) "#" ".")))))
+               (if (<= val idx (+ val 2))
+                 \█ \░)))))
 
-(let [lns (line-seq (java.io.BufferedReader. *in*))
-      ins (sequence (map parse-ln) lns)
-      res (loop [res [1] reg 1 [[wait change] & ins] ins]
-            (if (seq ins)
-              (recur (into res (repeat wait reg))
-                     (+ reg change)
-                     ins)
-              res))]
-  (println "Part A:"   (transduce (map #(* (res %) %)) + [20 60 100 140 180 220]))
-  (println "Part B:\n" (->> (sequence (comp (partition-all 40)
-                                            (map (partial transduce xf-render str)))
-                                      res)
-                            (join "\n"))))
+(defn solve [xs ln]
+  (let [prev (peek xs)]
+    (condp re-find ln
+      #"noop"              (conj xs prev)
+      #"addx (-?\d+)" :>> #(conj xs prev (+ prev (Integer. (second %)))))))
+
+(let [in (line-seq (java.io.BufferedReader. *in*))
+      xs (reduce solve [1 1] in)]
+  (println "Part A:" (reduce + (map (fn [x] (* (xs x) x)) [20 60 100 140 180 220])))
+  (println "Part B:")
+  (println (->> (partition 40 xs)
+                (map #(join "" (sequence render %)))
+                (join "\n"))))
