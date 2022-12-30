2f6a64f620994ba85047fa39497f7b11d473c253
Sam Ferrell <mail@samcf.me>
2022-12-30 14:52:11 -0700

    Improved solution.

--- 2020-01-report-repair.clj
+++ 2020-01-report-repair.clj
@@ -1,3 +1,3 @@
-(let [xs (->> (java.io.BufferedReader. *in*) (line-seq) (sequence (map (fn [x] (Integer. x)))))]
+(let [xs (into (sorted-set) (map #(Integer. %)) (line-seq (java.io.BufferedReader. *in*)))]
   (println "Part A:" (first (for [a xs b xs      :when (= (+ a b)   2020)] (* a b))))
   (println "Part B:" (first (for [a xs b xs c xs :when (= (+ a b c) 2020)] (* a b c)))))
