11fe2763f28c5876c37056e91ce5ddbbe18ee1b4
Sam Ferrell <mail@samcf.me>
2023-02-15 02:28:21 -0700

    Simplify solution for 2020, Day 10.

--- 2020-10-adapter-array.clj
+++ 2020-10-adapter-array.clj
@@ -1,6 +1,6 @@
 (defn diffs [xs]
-  (map (comp (partial apply -) reverse)
-       (partition 2 1 xs)))
+  (mapv (comp (partial apply -) reverse)
+        (partition 2 1 xs)))
 
 (defn arrangements [[h & t] a b c]
   (if (seq t)
@@ -10,7 +10,7 @@
     c))
 
 (let [in (line-seq (java.io.BufferedReader. *in*))
-      xs (into (sorted-set) (map #(Integer. %)) in)
-      xs (conj xs 0 (+ (apply max xs) 3))]
-  (println "Part A:" (->> (diffs xs) (frequencies) (vals) (apply *)))
-  (println "Part B:" (->  (diffs xs) (arrangements 0 0 1))))
+      xs (into (sorted-set 0) (map #(Integer. %)) in)
+      xs (conj (diffs xs) 3)]
+  (println "Part A:" (reduce * (vals (frequencies xs))))
+  (println "Part B:" (arrangements xs 0 0 1)))
