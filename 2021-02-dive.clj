904a119e0625eac2984bbc5348ebda5e4f108697
Sam Ferrell <mail@samcf.me>
2023-02-15 03:19:43 -0700

    Update for 2021, Day 2.

--- 2021-02-dive.clj
+++ 2021-02-dive.clj
@@ -1,28 +1,20 @@
 (require '[clojure.string :refer [split]])
 
-(defn a [coll]
-  (->> (reduce
-        (fn [[h d] [c n]]
-          (case c
-            "forward" [(+ h n) d]
-            "down"    [h (+ d n)]
-            "up"      [h (- d n)])) [0 0] coll)
-       (apply *)))
+(defn dive-a [[h d] [c n]]
+  (case c
+    "forward" [(+ h n) d]
+    "down"    [h (+ d n)]
+    "up"      [h (- d n)]))
 
-(defn b [coll]
-  (->> (reduce
-        (fn [[h d a] [c n]]
-          (case c
-            "forward" [(+ h n) (+ d (* a n)) a]
-            "down"    [h d (+ a n)]
-            "up"      [h d (- a n)])) [0 0 0] coll)
-       (take 2)
-       (apply *)))
+(defn dive-b [[h d a] [c n]]
+  (case c
+    "forward" [(+ h n) (+ d (* a n)) a]
+    "down"    [h d (+ a n)]
+    "up"      [h d (- a n)]))
 
-(def commands
-  (into []
-        (map #(let [[c n] (split % #" ")] [c (Integer. n)]))
-        (line-seq (java.io.BufferedReader. *in*))))
-
-(println "Part A:" (a commands))
-(println "Part B:" (b commands))
+(let [in (line-seq (java.io.BufferedReader. *in*))
+      cf (fn [[h d]] (* h d))
+      xf (comp (map (fn [s]     (split s #" ")))
+               (map (fn [[c v]] [c (Integer. v)])))]
+  (println "Part A:" (transduce xf (completing dive-a cf) [0 0]   in))
+  (println "Part B:" (transduce xf (completing dive-b cf) [0 0 0] in)))
