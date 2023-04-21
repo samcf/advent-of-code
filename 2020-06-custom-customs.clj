142eb4c6ea7259c219fcc6a9777f101acf9680e2
Sam Ferrell <mail@samcf.me>
2023-04-20 18:57:12 -0600

    Minor refactor for the 2020, Day 6 solution.

--- 2020-06-custom-customs.clj
+++ 2020-06-custom-customs.clj
@@ -1,10 +1,11 @@
 (require '[clojure.set :refer [union intersection]])
 
 (let [in (line-seq (java.io.BufferedReader. *in*))
-      xf (comp (partition-by (partial = ""))
-               (filter       (partial not= [""]))
-               (map          (partial map (partial into (hash-set)))))
-      xa (comp xf (map (partial apply union)) (map count))
-      xb (comp xf (map (partial apply intersection)) (map count))]
-  (println "Part A:" (transduce xa + in))
-  (println "Part B:" (transduce xb + in)))
+      xf (fn [f]
+           (comp (partition-by (partial = ""))
+                 (filter       (partial not= [""]))
+                 (map          (partial map (partial into (hash-set))))
+                 (map          (partial apply f))
+                 (map          count)))]
+  (println "Part A:" (transduce (xf union)        + in))
+  (println "Part B:" (transduce (xf intersection) + in)))
