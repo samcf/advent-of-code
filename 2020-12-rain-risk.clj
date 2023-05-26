8b58b7df42afdb07d64723c383e1d6e10009794d
Sam Ferrell <mail@samcf.me>
2023-05-26 17:21:18 -0600

    Simplify solution for 2020, Day 12.

--- 2020-12-rain-risk.clj
+++ 2020-12-rain-risk.clj
@@ -2,37 +2,34 @@
   (let [[_ c n] (re-find #"(\w){1}([0-9]+)" ln)]
     [(first c) (Integer. n)]))
 
-(def heading {0 [1 0] 90 [0 -1] 180 [-1 0] 270 [0 1] 360 [1 0]})
+(defn rotate [x y deg]
+  (case deg
+    90  [(* y -1) x]
+    180 [(* x -1) (* y -1)]
+    270 [y (* x -1)]))
 
-(defn solve-a [[ax ay hd] [cd mv]]
+(defn solve-a [[ax ay bx by] [cd vl]]
   (case cd
-    \N [ax (- ay mv) hd]
-    \E [(+ ax mv) ay hd]
-    \S [ax (+ ay mv) hd]
-    \W [(- ax mv) ay hd]
-    \L [ax ay (mod (+ hd mv) 360)]
-    \R [ax ay (mod (- hd mv) 360)]
-    \F (let [[ux uy] (heading hd)]
-         [(+ ax (* mv ux)) (+ ay (* mv uy)) hd])))
+    \N [ax (- ay vl) bx by]
+    \E [(+ ax vl) ay bx by]
+    \S [ax (+ ay vl) bx by]
+    \W [(- ax vl) ay bx by]
+    \L (let [[rx ry] (rotate bx by (- 360 vl))] [ax ay rx ry])
+    \R (let [[rx ry] (rotate bx by vl)] [ax ay rx ry])
+    \F [(+ ax (* vl bx)) (+ ay (* vl by)) bx by]))
 
-(defn rotate [x y deg dir]
-  (case [dir deg]
-    ([1 90] [-1 270])  [(* y -1) x]
-    ([1 180] [-1 180]) [(* x -1) (* y -1)]
-    ([1 270] [-1 90])  [y (* x -1)]))
-
-(defn solve-b [[ax ay bx by] [cd mv]]
+(defn solve-b [[ax ay bx by] [cd vl]]
   (case cd
-    \N [ax ay bx (- by mv)]
-    \E [ax ay (+ bx mv) by]
-    \S [ax ay bx (+ by mv)]
-    \W [ax ay (- bx mv) by]
-    \L (let [[rx ry] (rotate bx by mv -1)] [ax ay rx ry])
-    \R (let [[rx ry] (rotate bx by mv  1)] [ax ay rx ry])
-    \F [(+ ax (* mv bx)) (+ ay (* mv by)) bx by]))
+    \N [ax ay bx (- by vl)]
+    \E [ax ay (+ bx vl) by]
+    \S [ax ay bx (+ by vl)]
+    \W [ax ay (- bx vl) by]
+    \L (let [[rx ry] (rotate bx by (- 360 vl))] [ax ay rx ry])
+    \R (let [[rx ry] (rotate bx by vl)] [ax ay rx ry])
+    \F [(+ ax (* vl bx)) (+ ay (* vl by)) bx by]))
 
 (let [in (line-seq (java.io.BufferedReader. *in*))
       cf (fn [[x y]] (+ (abs x) (abs y)))
       rf (fn [f] (completing f cf))]
-  (println "Part A:" (transduce (map parse) (rf solve-a) [0 0 0] in))
+  (println "Part A:" (transduce (map parse) (rf solve-a) [0 0  1  0] in))
   (println "Part A:" (transduce (map parse) (rf solve-b) [0 0 10 -1] in)))
