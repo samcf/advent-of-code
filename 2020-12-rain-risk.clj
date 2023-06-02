195e762ab5d7e6fcbe4e15a9cc448e7fe8be0350
Sam Ferrell <mail@samcf.me>
2023-06-01 20:33:36 -0600

    Minor change for solution for 2020, Day 12.

--- 2020-12-rain-risk.clj
+++ 2020-12-rain-risk.clj
@@ -8,28 +8,30 @@
     180 [(* x -1) (* y -1)]
     270 [y (* x -1)]))
 
-(defn solve-a [[ax ay bx by] [cd vl]]
-  (case cd
-    \N [ax (- ay vl) bx by]
-    \E [(+ ax vl) ay bx by]
-    \S [ax (+ ay vl) bx by]
-    \W [(- ax vl) ay bx by]
-    \L (let [[rx ry] (rotate bx by (- 360 vl))] [ax ay rx ry])
-    \R (let [[rx ry] (rotate bx by vl)] [ax ay rx ry])
-    \F [(+ ax (* vl bx)) (+ ay (* vl by)) bx by]))
+(defn solve-a
+  ([[ax ay _ _]] (+ (abs ax) (abs ay)))
+  ([[ax ay bx by] [cd vl]]
+   (case cd
+     \N [ax (- ay vl) bx by]
+     \E [(+ ax vl) ay bx by]
+     \S [ax (+ ay vl) bx by]
+     \W [(- ax vl) ay bx by]
+     \L (let [[rx ry] (rotate bx by (- 360 vl))] [ax ay rx ry])
+     \R (let [[rx ry] (rotate bx by vl)] [ax ay rx ry])
+     \F [(+ ax (* vl bx)) (+ ay (* vl by)) bx by])))
 
-(defn solve-b [[ax ay bx by] [cd vl]]
-  (case cd
-    \N [ax ay bx (- by vl)]
-    \E [ax ay (+ bx vl) by]
-    \S [ax ay bx (+ by vl)]
-    \W [ax ay (- bx vl) by]
-    \L (let [[rx ry] (rotate bx by (- 360 vl))] [ax ay rx ry])
-    \R (let [[rx ry] (rotate bx by vl)] [ax ay rx ry])
-    \F [(+ ax (* vl bx)) (+ ay (* vl by)) bx by]))
+(defn solve-b
+  ([[ax ay _ _]] (+ (abs ax) (abs ay)))
+  ([[ax ay bx by] [cd vl]]
+   (case cd
+     \N [ax ay bx (- by vl)]
+     \E [ax ay (+ bx vl) by]
+     \S [ax ay bx (+ by vl)]
+     \W [ax ay (- bx vl) by]
+     \L (let [[rx ry] (rotate bx by (- 360 vl))] [ax ay rx ry])
+     \R (let [[rx ry] (rotate bx by vl)] [ax ay rx ry])
+     \F [(+ ax (* vl bx)) (+ ay (* vl by)) bx by])))
 
-(let [in (line-seq (java.io.BufferedReader. *in*))
-      cf (fn [[x y]] (+ (abs x) (abs y)))
-      rf (fn [f] (completing f cf))]
-  (println "Part A:" (transduce (map parse) (rf solve-a) [0 0  1  0] in))
-  (println "Part B:" (transduce (map parse) (rf solve-b) [0 0 10 -1] in)))
+(let [in (line-seq (java.io.BufferedReader. *in*))]
+  (println "Part A:" (transduce (map parse) solve-a [0 0  1  0] in))
+  (println "Part B:" (transduce (map parse) solve-b [0 0 10 -1] in)))
