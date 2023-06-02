291f461f113190c0f0988894dce21ffe26c25f77
Sam Ferrell <mail@samcf.me>
2023-06-01 20:27:47 -0600

    Part 2 solution for 2020, Day 13.

--- 2020-13-shuttle-search.clj
+++ 2020-13-shuttle-search.clj
@@ -1,6 +1,24 @@
-(let [[ts xs] (line-seq (java.io.BufferedReader. *in*))
-      ts (Integer. ts)
-      rm (fn [x] [(- x (mod ts x)) x])
-      xs (sequence (map #(Integer. %)) (re-seq #"\d+" xs))
-      [[rs id] & _] (into (sorted-map) (map rm) xs)]
-  (println "Part A:" (* rs id)))
+(def parse-a
+  (fn [t]
+    (comp (filter (fn [x] (not= x "x")))
+          (map (fn [x] (Integer. x)))
+          (map (juxt (fn [x] (- x (mod t x))) identity)))))
+
+(def parse-b
+  (comp (map-indexed vector)
+        (filter (fn [[_ x]] (not= x "x")))
+        (map (fn [[i x]] [i (Integer. x)]))))
+
+(defn repeats-at
+  ([[t _]] t)
+  ([[t a] [o b]]
+   (loop [x (+ t a) y 0]
+     (case [(zero? (mod (+ x o) b)) (pos? y)]
+       [false false] (recur (+ x a) 0)
+       [false  true] (recur (+ x a) y)
+       [true  false] (recur (+ x a) x)
+       [true   true] [y (- x y)]))))
+
+(let [[t b] (line-seq (java.io.BufferedReader. *in*))]
+  (println "Part A:" (->> (re-seq #"\d+|x" b) (into (sorted-map) (parse-a (Integer. t))) (first) (reduce *)))
+  (println "Part B:" (->> (re-seq #"\d+|x" b) (transduce parse-b repeats-at [0 1]))))
