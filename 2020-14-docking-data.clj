87d9de3e9113bca6fb3e3fcbc037a4ab5d86077a
Sam Ferrell <mail@samcf.me>
2023-09-13 16:16:15 -0600

    Clean up solution for Day 14, 2020.

--- 2020-14-docking-data.clj
+++ 2020-14-docking-data.clj
@@ -1,22 +1,21 @@
 (require '[clojure.string :refer [replace]])
 
-(defn parse-assign [s]
-  (let [[_ a v] (re-find #"mem\[(\d+)\] = (\d+)" s)]
-    [(Integer. a) (Integer. v)]))
-
-(defn parse [[[mask] xs]]
-  (let [o-mask (Long/parseLong (replace (subs mask 7) #"X"  {"X" "0"}) 2)
-        z-mask (Long/parseLong (replace (subs mask 7) #"\w" {"X" "0" "1" "0" "0" "1"}) 2)]
-    (into [o-mask z-mask] (map parse-assign) xs)))
+(defn parse [s]
+  (condp re-find s
+    #"mem\[(\d+)\] = (\d+)" :>> (fn [[_ x y]] [(Integer. x) (Integer. y)])
+    #"mask = (\w+)"         :>> (fn [[_ x]] x)))
 
 (defn solve-a
   ([r] (transduce (map val) + r))
-  ([r [o z & xs]]
-   (let [f (fn [[_ v]] (- (bit-or v o) (bit-and v z)))]
+  ([r [s & xs]]
+   (let [o (Long/parseLong (replace s #"X" "0") 2)
+         z (Long/parseLong (replace s #"[X10]" {"X" "0" "1" "0" "0" "1"}) 2)
+         f (fn [[_ v]] (- (bit-or v o) (bit-and v z)))]
      (into r (map (juxt first f)) xs))))
 
 (let [in (line-seq (java.io.BufferedReader. *in*))
-      xf (comp (partition-by (fn [s] (= (subs s 0 4) "mask")))
+      xf (comp (map parse)
+               (partition-by string?)
                (partition-all 2)
-               (map parse))]
-  (println "Part A:" (transduce xf solve-a {} in)))
+               (map (partial apply into)))]
+  (time (println "Part A:" (transduce xf solve-a {} in))))
