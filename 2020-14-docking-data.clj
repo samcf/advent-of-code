a38bdd8a8b23708eaffb6312f890b92be5c0798f
Sam Ferrell <mail@samcf.me>
2023-09-15 15:32:55 -0600

    Improved solution for Day 14, 2020.

--- 2020-14-docking-data.clj
+++ 2020-14-docking-data.clj
@@ -1,54 +1,49 @@
-(require '[clojure.string :refer [replace]]
-         '[clojure.math.combinatorics :refer [cartesian-product]])
-
-(defn partition-before [pred]
-  (comp (partition-by pred) (partition-all 2) (map (partial apply into))))
-
 (defn parse [s]
   (condp re-find s
     #"mem\[(\d+)\] = (\d+)" :>> (fn [[_ x y]] [(Integer. x) (Integer. y)])
-    #"mask = (\w+)"         :>> (fn [[_ x]] x)))
+    #"mask = (\w+)"         :>> (comp seq second)))
 
-(defn apply-mask [s x]
-  (let [a (Long/parseLong (replace s #"X" "0") 2)
-        b (Long/parseLong (replace s #"[X10]" {"X" "0" "1" "0" "0" "1"}) 2)]
-    (- (bit-or x a) (bit-and x b))))
-
-(defn zip-replace [pred xs ys]
-  (loop [xs xs ys ys rs []]
-    (if (seq xs)
-      (if (seq ys)
-        (if (pred (first xs))
-          (recur (rest xs) (rest ys) (conj rs (first ys)))
-          (recur (rest xs) ys (conj rs (first xs))))
-        (into rs xs))
-      rs)))
-
-(defn combinations [mask]
-  (let [xs (get (frequencies mask) \X)
-        xf (comp (map (partial zip-replace #{\X} mask))
-                 (map (partial apply str)))]
-    (sequence xf (apply cartesian-product (repeat xs [\1 \0])))))
+(defn mask-a [xs]
+  (fn [v]
+    (loop [xs xs v v]
+      (if (seq xs)
+        (let [[x & xs] xs i (count xs)]
+          (case x
+            \0 (recur xs (bit-clear v i))
+            \1 (recur xs (bit-set v i))
+            \X (recur xs v))) v))))
 
 (defn solve-a
-  ([r] (transduce (map val) + r))
-  ([r [s & xs]]
-   (let [f (fn [[_ x]] (apply-mask s x))]
-     (into r (map (juxt first f)) xs))))
+  ([result]
+   (transduce (map val) + result))
+  ([result [mask & assigns]]
+   (let [xf (map (juxt first (comp (mask-a mask) second)))]
+     (into result xf assigns))))
+
+(defn mask-b [acc xs v]
+  (if (seq xs)
+    (let [[x & xs] xs i (count xs)]
+      (case x
+        \0 (mask-b acc xs v)
+        \1 (mask-b acc xs (bit-set v i))
+        \X (let [a (mask-b acc xs (bit-clear v i))
+                 b (mask-b acc xs (bit-set v i))]
+             (into (into acc a) b))))
+    (conj acc v)))
 
 (defn solve-b
-  ([r] (transduce (map val) + r))
-  ([r [s & xs]]
-   (let [a (Long/parseLong (replace s #"X" "0") 2)
-         c (combinations (replace s #"0" "Y"))
-         f (fn [[k x]]
-             (let [xf (comp (map (fn [s] (replace s #"Y" "X")))
-                            (map (fn [s] (bit-or (apply-mask s k) a)))
-                            (map (juxt identity (constantly x))))]
-               (sequence xf c)))]
-     (into r (mapcat f) xs))))
+  ([result]
+   (transduce (map val) + result))
+  ([result [mask & assigns]]
+   (into result
+         (comp (map (fn [[k v]] [(mask-b [] mask k) v]))
+               (map (fn [[k v]] (map (fn [k] [k v]) k)))
+               cat) assigns)))
 
 (let [in (line-seq (java.io.BufferedReader. *in*))
-      xf (comp (map parse) (partition-before string?))]
+      xf (comp (map parse)
+               (partition-by seq?)
+               (partition-all 2)
+               (map (partial apply into)))]
   (println "Part A:" (transduce xf solve-a {} in))
   (println "Part B:" (transduce xf solve-b {} in)))
