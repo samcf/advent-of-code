da077798a2d304879b95cfe4d48ff9da6f50400f
Sam Ferrell <mail@samcf.me>
2023-09-20 20:43:25 -0600

    Solution for Part 2 of Day 16, 2020.

--- 2020-16-ticket-translation.clj
+++ 2020-16-ticket-translation.clj
@@ -1,21 +1,45 @@
-(def parse-rule #"([\w\s]+): (\d+)-(\d+) or (\d+)-(\d+)")
-(def parse-tick #"(\d+)")
+(require '[clojure.set :refer [difference]])
+
+(def re-rule #"([\w\s]+): (\d+)-(\d+) or (\d+)-(\d+)")
+(def re-tick #"(\d+)")
 
 (defn parse [s]
   (condp re-seq s
-    parse-rule :>> (fn [[[_ s & xs]]] (into [s] (map #(Integer. %)) xs))
-    parse-tick :>> (fn [xs] (sequence (map (fn [[_ x]] (Integer. x))) xs)) ""))
+    re-rule :>> (fn [[[_ s & xs]]] (into [s] (map #(Integer. %)) xs))
+    re-tick :>> (fn [xs] (sequence (map (fn [[_ x]] (Integer. x))) xs))
+    nil))
 
-(defn valid? [rules x]
-  (if (seq rules)
-    (let [[[_ a b c d] & rules] rules]
-      (if (not (or (<= a x b) (<= c x d)))
-        (recur rules x) true)) false))
+(defn within? [[_ a b c d] x]
+  (or (<= a x b) (<= c x d)))
+
+(defn valid? [rules]
+  (fn [x]
+    (some (fn [rule] (within? rule x)) rules)))
+
+(defn candidate? [xs]
+  (fn [rule]
+    (every? (fn [x] (within? rule x)) xs)))
+
+(defn fields [columns rules]
+  (let [xf (fn [xs] (comp (filter (candidate? xs)) (map first)))
+        xs (sequence (comp (map (fn [xs] (into #{} (xf xs) rules))) (map-indexed vector)) columns)]
+    (loop [poss (sort-by (comp count second) xs)
+           path (into [] (repeat 20 0))
+           seen #{}]
+      (if (seq poss)
+        (let [[indx rules] (first poss)
+              rule (first (difference rules seen))]
+          (recur (rest poss) (assoc path indx rule) (conj seen rule)))
+        path))))
 
 (let [in (line-seq (java.io.BufferedReader. *in*))
       xf (comp (map parse)
-               (filter (complement #{""}))
+               (filter (complement nil?))
                (partition-by (comp string? first)))
-      [rs [_ & nt]] (sequence xf in)
-      xt (comp cat (filter (complement (partial valid? rs))))]
-  (println "Part A:" (transduce xt + nt)))
+      [rules [xs & ys]] (sequence xf in)
+      vs (filter (partial every? (valid? rules)) ys)]
+  (println "Part A:" (transduce (comp cat (filter (complement (valid? rules)))) + ys))
+  (time (println "Part B:" (transduce (comp (filter (comp (partial re-find #"departure") key)) (map val)) *
+                                      (-> (apply mapv vector vs)
+                                          (fields rules)
+                                          (zipmap xs))))))
