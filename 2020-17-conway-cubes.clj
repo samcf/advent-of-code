e158080f376369fda54334c4e149de6964bf457d
Sam Ferrell <mail@samcf.me>
2023-09-22 23:02:49 -0600

    Updates for Day 17, 2020.

--- 2020-17-conway-cubes.clj
+++ 2020-17-conway-cubes.clj
@@ -1,16 +1,25 @@
-(require '[clojure.set :refer [difference]])
+(defn parse [n]
+  (comp (map-indexed (fn [y r] (map-indexed (fn [x v] [(into [x y] (repeat n 0)) v]) r))) cat
+        (filter (comp #{\#} second))))
 
-(defn neighbors3 [v]
-  (let [[ax ay az] v]
-    (for [bx [-1 0 1] by [-1 0 1] bz [-1 0 1]
-          :let  [b [(+ ax bx) (+ ay by) (+ az bz)]]
-          :when (not= v b)] b)))
+(defn neighbors3 [from]
+  (let [[ax ay az] from]
+    (for [bx [-1 0 1]
+          by [-1 0 1]
+          bz [-1 0 1]
+          :let  [next [(+ ax bx) (+ ay by) (+ az bz)]]
+          :when (not= from next)]
+      next)))
 
-(defn neighbors4 [v]
-  (let [[ax ay az ah] v]
-    (for [bx [-1 0 1] by [-1 0 1] bz [-1 0 1] bh [-1 0 1]
-          :let  [b [(+ ax bx) (+ ay by) (+ az bz) (+ ah bh)]]
-          :when (not= v b)] b)))
+(defn neighbors4 [from]
+  (let [[ax ay az ah] from]
+    (for [bx [-1 0 1]
+          by [-1 0 1]
+          bz [-1 0 1]
+          bh [-1 0 1]
+          :let  [next [(+ ax bx) (+ ay by) (+ az bz) (+ ah bh)]]
+          :when (not= from next)]
+      next)))
 
 (def neighbors
   (memoize
@@ -20,23 +29,23 @@
        (neighbors4 v)))))
 
 (defn turn [xs [k v]]
-  (let [xf (comp (map xs) (filter #{\#}))
-        ct (count (take 4 (sequence xf (neighbors k))))]
+  (let [xf (comp (map xs) (filter #{\#}) (take 4))
+        ct (count (sequence xf (neighbors k)))]
     (cond (and (= v \#) (<= 2 ct 3)) \#
           (and (= v \.) (= ct 3))    \#
           :else \.)))
 
 (defn step [xs]
-  (let [nf (comp (filter (comp #{\#} val)) (map key) (mapcat neighbors)) ;; find all neighbors for all active cubes
-        uk (difference (into #{} nf xs) (set (keys xs)))                 ;; find the difference from existing keys
-        nk (sequence (map (juxt identity (constantly \.))) uk)           ;; turn these into key/value pairs
-        xf (map (juxt first (partial turn xs)))]                         ;; turn existing cubes
-    (into xs xf (into nk xs))))
+  (let [vs (vec xs)
+        ks (set (keys xs))
+        nf (comp (mapcat (comp neighbors key))
+                 (distinct)
+                 (filter (complement ks))
+                 (map (juxt identity (constantly \.))))
+        xf (comp (map (juxt first (partial turn xs)))
+                 (filter (comp #{\#} second)))]
+    (into {} xf (into vs nf vs))))
 
-(defn active [xs]
-  (transduce (map (fn [[_ v]] (if (= v \#) 1 0))) + xs))
-
-(let [in (line-seq (java.io.BufferedReader. *in*))
-      xf (fn [n] (comp (map-indexed (fn [y r] (map-indexed (fn [x v] [(into [x y] (repeat n 0)) v]) r))) cat))]
-  (println "Part A:" (active (nth (iterate step (into {} (xf 1) in)) 6)))
-  (println "Part B:" (active (nth (iterate step (into {} (xf 2) in)) 6))))
+(let [in (line-seq (java.io.BufferedReader. *in*))]
+  (println "Part A:" (count (nth (iterate step (into {} (parse 1) in)) 6)))
+  (println "Part B:" (count (nth (iterate step (into {} (parse 2) in)) 6))))
