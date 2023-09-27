b001a7e2c2e609a2f61e81721acf43ba0ee24fa8
Sam Ferrell <mail@samcf.me>
2023-09-27 12:58:39 -0600

    Solution for Part 2 of Day 18, 2020.

--- 2020-18-operation-order.clj
+++ 2020-18-operation-order.clj
@@ -1,18 +1,36 @@
 (require '[clojure.string :refer [replace]])
 
-(defn solve [xs]
+(defn token [c]
+  (if (#{\( \) \* \+} c) c (Integer. (str c))))
+
+(def parse
+  (comp (map (fn [xs] (replace xs #" " "")))
+        (map (fn [xs] (map token xs)))))
+
+(defn evaluate [xs]
   (loop [xs xs rs 0 op +]
     (if (seq xs)
       (let [[x & xs] xs]
         (case x
-          \( (let [[r xs] (solve xs)] (recur xs (op rs r) op))
+          \( (let [[rx xs] (evaluate xs)] (recur xs (op rs rx) op))
           \) [rs xs]
           \+ (recur xs rs +)
           \* (recur xs rs *)
-          (recur xs (op rs (Integer. (str x))) op))) rs)))
+          (recur xs (op rs x) op))) rs)))
+
+(defn rewrite [xs rs]
+  (if (seq xs)
+    (let [[x & xs] xs]
+      (case x
+        \( (let [[xs ys] (rewrite xs [])] (recur xs (conj rs (evaluate ys))))
+        \) [xs rs]
+        \+ (if (= (first xs) \()
+             (let [[xs ys] (rewrite (rest xs) [])]
+               (recur xs (conj (pop rs) (+ (peek rs) (evaluate ys)))))
+             (recur (rest xs) (conj (pop rs) (+ (peek rs) (first xs)))))
+        (recur xs (conj rs x)))) rs))
 
 (let [in (line-seq (java.io.BufferedReader. *in*))
-      xf (comp (map (fn [s] (replace s #" " "")))
-               (map seq)
-               (map solve))]
-  (println "Part A:" (transduce xf + in)))
+      xf (fn [f] (comp parse (map f) (map evaluate)))]
+  (println "Part A:" (transduce (xf identity) + in))
+  (println "Part B:" (transduce (xf (fn [xs] (rewrite xs []))) + in)))
