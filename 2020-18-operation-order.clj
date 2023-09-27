495e2fc8b41856a9ff531fd89d1ffb36f79c43cd
Sam Ferrell <mail@samcf.me>
2023-09-27 17:59:29 -0600

    Simplify solution for Day 18, 2020.

--- 2020-18-operation-order.clj
+++ 2020-18-operation-order.clj
@@ -1,11 +1,4 @@
-(require '[clojure.string :refer [replace]])
-
-(defn token [c]
-  (if (#{\( \) \* \+} c) c (Integer. (str c))))
-
-(def parse
-  (comp (map (fn [xs] (replace xs #" " "")))
-        (map (fn [xs] (map token xs)))))
+(def token {\( \( \) \) \+ \+ \* \* \1 1 \2 2 \3 3 \4 4 \5 5 \6 6 \7 7 \8 8 \9 9 \0 0})
 
 (defn evaluate [xs]
   (loop [xs xs rs 0 op +]
@@ -18,19 +11,22 @@
           \* (recur xs rs *)
           (recur xs (op rs x) op))) rs)))
 
-(defn rewrite [xs rs]
-  (if (seq xs)
-    (let [[x & xs] xs]
-      (case x
-        \( (let [[xs ys] (rewrite xs [])] (recur xs (conj rs (evaluate ys))))
-        \) [xs rs]
-        \+ (if (= (first xs) \()
-             (let [[xs ys] (rewrite (rest xs) [])]
-               (recur xs (conj (pop rs) (+ (peek rs) (evaluate ys)))))
-             (recur (rest xs) (conj (pop rs) (+ (peek rs) (first xs)))))
-        (recur xs (conj rs x)))) rs))
+(defn rewrite
+  ([xs] (rewrite xs []))
+  ([xs rs]
+   (if (seq xs)
+     (let [[x & xs] xs]
+       (case x
+         \( (let [[xs ys] (rewrite xs)] (recur xs (conj rs (evaluate ys))))
+         \) [xs rs]
+         \+ (if (= (first xs) \()
+              (let [[xs ys] (rewrite (rest xs))]
+                (recur xs (conj (pop rs) (+ (peek rs) (evaluate ys)))))
+              (recur (rest xs) (conj (pop rs) (+ (peek rs) (first xs)))))
+         (recur xs (conj rs x)))) rs)))
 
 (let [in (line-seq (java.io.BufferedReader. *in*))
-      xf (fn [f] (comp parse (map f) (map evaluate)))]
+      pf (comp (filter (complement #{\space})) (map token))
+      xf (fn [f] (comp (map (fn [xs] (sequence pf xs))) (map f) (map evaluate)))]
   (println "Part A:" (transduce (xf identity) + in))
-  (println "Part B:" (transduce (xf (fn [xs] (rewrite xs []))) + in)))
+  (println "Part B:" (transduce (xf rewrite) + in)))
