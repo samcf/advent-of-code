d02e378eeca0609f4b70dcaba11cbfafab90f637
Sam Ferrell <mail@samcf.me>
2023-12-02 00:37:52 -0700

    Simplify Day 2, 2023 using `merge-with`.

--- 2023-02-cube-conundrum.clj
+++ 2023-02-cube-conundrum.clj
@@ -4,32 +4,33 @@
 (def re-hand #"(\d+) (red|green|blue)")
 
 (defn parse-hand [ln]
-  (sequence (map (fn [[_ n c]] (list (keyword c) (parse-long n))))
+  (sequence (map (fn [[_ n c]] {(keyword c) (parse-long n)}))
             (re-seq re-hand ln)))
-
-(defn totals [pairs]
-  (reduce (fn [counts [color count]]
-            (update counts color (partial + count)))
-          {:red 0 :blue 0 :green 0}
-          pairs))
 
 (defn parse [ln]
   (let [[[_ id ln]] (re-seq re-game ln)]
-    (list (parse-long id)
-          (sequence (comp (map parse-hand) (map totals))
-                    (split ln #"; ")))))
+    [(parse-long id)
+     (sequence (comp (map parse-hand)
+                     (map (partial apply merge-with + {:red 0 :blue 0 :green 0})))
+               (split ln #"; "))]))
 
 (defn valid? [hand]
-  (and (<= (get hand :red   0) 12)
-       (<= (get hand :blue  0) 14)
-       (<= (get hand :green 0) 13)))
+  (and (<= (:red   hand) 12)
+       (<= (:blue  hand) 14)
+       (<= (:green hand) 13)))
 
-(defn power [hands]
-  (->> (map (juxt :red :blue :green) hands)
-       (apply mapv vector)
-       (map (partial apply max))
-       (apply *)))
+(def xf-part-a
+  (comp (map parse)
+        (filter (comp (partial every? valid?) second))
+        (map first)))
+
+(def xf-part-b
+  (comp (map parse)
+        (map second)
+        (map (partial apply merge-with max))
+        (map vals)
+        (map (partial reduce *))))
 
 (let [in (line-seq (java.io.BufferedReader. *in*))]
-  (println "Part A:" (transduce (comp (map parse) (filter (comp (partial every? valid?) second)) (map first)) + in))
-  (println "Part B:" (transduce (comp (map parse) (map (comp power second))) + in)))
+  (println "Part A:" (transduce xf-part-a + in))
+  (println "Part B:" (transduce xf-part-b + in)))
