6f26514c8eb01daf1014950f83e7491a66668f46
Sam Ferrell <mail@samcf.me>
2023-12-08 13:33:24 -0700

    Refactor for Day 8, 2023.

--- 2023-08-haunted-wasteland.clj
+++ 2023-08-haunted-wasteland.clj
@@ -3,25 +3,23 @@
     (if (> (mod x b) 0)
       (recur (+ x a)) x)))
 
-(defn steps [graph instr pred start]
-  (loop [xs (seq instr) node start steps 0]
+(defn steps [graph ins pred start]
+  (loop [in (seq ins) node start steps 0]
     (if (not (pred node))
-      (if (seq xs)
-        (let [[dir & xs] xs]
+      (if (seq in)
+        (let [[dir & xs] in]
           (recur xs (get graph [node dir]) (inc steps)))
-        (recur (seq instr) node steps)) steps)))
+        (recur (seq ins) node steps)) steps)))
 
 (defn create-branch [[x l r]]
   [[[x \L] l] [[x \R] r]])
 
 (let [[xs _ & ys] (line-seq (java.io.BufferedReader. *in*))
-      parse (comp (map (partial re-seq #"[A-Z0-9]+"))
-                  (mapcat create-branch))
+      parse (comp (map (partial re-seq #"[A-Z0-9]+")) (mapcat create-branch))
       graph (into {} parse ys)
-      steps (partial steps graph xs)]
+      steps (partial steps graph xs)
+      solve (comp (map key) (map first)
+                  (filter (comp #{\A} last))
+                  (map (partial steps (comp #{\Z} last))))]
   (println "Part A:" (steps #{"ZZZ"} "AAA"))
-  (println "Part B:" (transduce (map (partial steps (comp #{\Z} last)))
-                                (completing converges identity) 1
-                                (sequence (comp (map (comp first key))
-                                                (filter (comp #{\A} last)))
-                                          graph))))
+  (println "Part B:" (transduce solve (completing converges identity) 1 graph)))
