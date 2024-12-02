70b79da54999adabf2d65eaf535c5bd2f07a1c6b
Sam Ferrell <mail@samcf.me>
2024-12-02 14:50:30 -0700

    Simplify solution for Day 1, 2024.

--- 2024-01-historian-hysteria.clj
+++ 2024-01-historian-hysteria.clj
@@ -1,12 +1,10 @@
-(require '[net.cgrand.xforms.io :as io]
-         '[net.cgrand.xforms :as x])
-
-(def xf (comp (mapcat (fn [ln] (re-seq #"\d+" ln)))
-              (map parse-long)
-              (x/multiplex {:xs (take-nth 2) :ys (comp (drop 1) (take-nth 2))})
-              (x/by-key (comp (x/sort) (x/into [])))))
-
-(let [{:keys [xs ys]} (into {} xf (io/lines-in *in*))]
-  (println "Part A:" (reduce + (map (comp abs -) xs ys)))
-  (println "Part B:" (let [frqs (frequencies ys)]
-                       (transduce (map (fn [x] (* (get frqs x 0) x))) + xs))))
+(let [in (line-seq (java.io.BufferedReader. *in*))
+      xf (fn [f]
+           (comp (map (fn [ln] (re-seq #"\d+" ln)))
+                 (map f)
+                 (map parse-long)))
+      xs (sort (into [] (xf first)  in))
+      ys (sort (into [] (xf second) in))
+      fq (frequencies ys)]
+  (println "Part A:" (reduce +  (map (comp abs -) xs ys)))
+  (println "Part B:" (transduce (map (fn [x] (* (fq x 0) x))) + xs)))
