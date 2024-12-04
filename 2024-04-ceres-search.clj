452c2f0ccfd4b700876f40c06ced890da5cbc608
Sam Ferrell <mail@samcf.me>
2024-12-04 13:25:30 -0700

    Improve solution for Day 4, 2024.

--- 2024-04-ceres-search.clj
+++ 2024-04-ceres-search.clj
@@ -1,25 +1,29 @@
-(defn count-a [xs idx len]
-  (let [offs [(- len) (inc (- len)) 1 (inc len) len (dec len) -1 (dec (- len))]
-        iter (range 1 4)
-        xfrm (comp (map (fn [offs] (map (fn [itr] (* offs itr)) iter)))
-                   (map (fn [offs] (map (fn [off] (+ idx off)) offs)))
-                   (map (fn [idxs] (map (fn [idx] (get xs idx)) idxs)))
-                   (map (fn [vals] (apply str vals)))
-                   (filter #{"MAS"})
-                   (map (constantly 1)))]
-    (transduce xfrm + offs)))
+(def cards [[0 -1] [1  0] [0 1] [-1 0]])
+(def diags [[-1 -1] [1 -1] [-1 1] [1 1]])
 
-(defn count-b [xs idx len]
-  (let [offs [(dec (- len)) (inc (- len)) (dec len) (inc len)]
-        vals (map (fn [off] (get xs (+ idx off))) offs)]
-    (if (#{"SSMM" "SMSM" "MMSS" "MSMS"} (apply str vals))
+(defn offsets [pairs len]
+  (map (fn [[x y]] (+ x (* y len))) pairs))
+
+(defn matches-a [xs idx offsets]
+  (reduce + (for [offset      offsets
+                  [step test] [[1 \M] [2 \A] [3 \S]]
+                  :let        [next (get xs (+ idx (* step offset)))]
+                  :while      (= next test)
+                  :when       (= step 3)]
+              1)))
+
+(defn matches-b [xs idx offsets]
+  (let [xf (comp (map (fn [x] (+ idx x))) (map (fn [i] (get xs i))))
+        xs (into [] xf offsets)]
+    (if (#{"SSMM" "SMSM" "MMSS" "MSMS"} (apply str xs))
       1 0)))
 
 (let [in (line-seq (java.io.BufferedReader. *in*))
+      ln (inc (count (first in)))
       xs (into [] (mapcat (fn [s] (str s \.))) in)
       rs (range (count xs))
-      xf (fn [c f]
-           (comp (filter (fn [idx] (= (get xs idx) c)))
-                 (map    (fn [idx] (f xs idx (inc (count (first in))))))))]
-  (println "Part A:" (transduce (xf \X count-a) + rs))
-  (println "Part B:" (transduce (xf \A count-b) + rs)))
+      xf (fn [f char offsets]
+           (comp (filter (fn [idx] (= (get xs idx) char)))
+                 (map    (fn [idx] (f xs idx offsets)))))]
+  (println "Part A:" (transduce (xf matches-a \X (offsets (into cards diags) ln)) + rs))
+  (println "Part B:" (transduce (xf matches-b \A (offsets diags ln)) + rs)))
