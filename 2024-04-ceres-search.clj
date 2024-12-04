d51d1c9a0fd215e23d5b47265f22672058317232
Sam Ferrell <mail@samcf.me>
2024-12-04 16:58:17 -0700

    Improve performance for Day 4, 2024.

--- 2024-04-ceres-search.clj
+++ 2024-04-ceres-search.clj
@@ -1,22 +1,21 @@
 (def cards [[0 -1] [1  0] [0 1] [-1 0]])
 (def diags [[-1 -1] [1 -1] [-1 1] [1 1]])
+(def steps (list 1 2 3))
+(def cross #{-990226162 1589543312 -411337452 1188667126})
 
 (defn offsets [pairs len]
   (map (fn [[x y]] (+ x (* y len))) pairs))
 
 (defn matches-a [xs idx offsets]
-  (reduce + (for [offset      offsets
-                  [step test] [[1 \M] [2 \A] [3 \S]]
-                  :let        [next (get xs (+ idx (* step offset)))]
-                  :while      (= next test)
-                  :when       (= step 3)]
+  (reduce + (for [offset offsets
+                  step steps
+                  :let   [val (get xs (+ idx (* step offset)))]
+                  :while (case step 1 (= val \M) 2 (= val \A) 3 (= val \S))
+                  :when  (= step 3)]
               1)))
 
 (defn matches-b [xs idx offsets]
-  (let [xf (comp (map (fn [x] (+ idx x))) (map (fn [i] (get xs i))))
-        xs (into [] xf offsets)]
-    (if (#{"SSMM" "SMSM" "MMSS" "MSMS"} (apply str xs))
-      1 0)))
+  (if (cross (hash (map (fn [off] (get xs (+ idx off))) offsets))) 1 0))
 
 (let [in (line-seq (java.io.BufferedReader. *in*))
       ln (inc (count (first in)))
