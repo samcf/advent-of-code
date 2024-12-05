5a156783c86b0e339db23029b99dff3208957485
Sam Ferrell <mail@samcf.me>
2024-12-05 02:18:46 -0700

    Comments for Day 5, 2024.

--- 2024-05-print-queue.clj
+++ 2024-05-print-queue.clj
@@ -1,3 +1,9 @@
+(def parse-xf
+  (comp (map (fn [ln] (re-seq #"\d+" ln)))
+        (map (fn [xs] (map parse-long xs)))
+        (partition-by (complement seq))
+        (filter (comp seq first))))
+
 (defn ordered-fn? [rules]
   (fn [xs]
     (if (seq xs)
@@ -12,19 +18,25 @@
 (defn mid [xs]
   (nth xs (/ (count xs) 2)))
 
-(let [lines (line-seq (java.io.BufferedReader. *in*))
-      parse (comp (map (fn [ln] (re-seq #"\d+" ln)))
-                  (map (fn [xs] (map parse-long xs)))
-                  (partition-by (complement seq))
-                  (filter (comp seq first)))
-      parts (into [] parse lines)
-      rules (reduce (fn [m [x y]]
-                      (if (m x)
-                        (update m x conj y)
-                        (update m x hash-set y))) {} (first parts))
-      updts (map (juxt (ordered-fn? rules) identity) (second parts))
-      xform (fn [f m] (comp (filter f) (map second) (map m) (map mid)))]
-  (println "Part A:" (transduce (xform first identity) + updts))
-  (println "Part B:" (transduce (xform (complement first)
-                                       (partial sort (compare-fn rules)))
-                                + updts)))
+(let [in (line-seq (java.io.BufferedReader. *in*))
+
+      ;; parse the input into two parts, the rules and the
+      ;; update lists
+      rs (into [] parse-xf in)
+
+      ;; create a map of rules whose keys are page numbers
+      ;; and who values are sets of pages that must appear
+      ;; after
+      ru (reduce (fn [m [x y]] (update m x (fnil conj #{}) y)) {} (first rs))
+
+      ;; describe each update list as either ordered or not
+      xs (map (juxt (ordered-fn? ru) identity) (second rs))
+
+      ;; pick out the appropriate update list, do any
+      ;; necessary transforms on it, and return the middle
+      ;; element
+      xf (fn [f m] (comp (filter f) (map second) (map m) (map mid)))]
+  (println "Part A:" (transduce (xf first identity) + xs))
+  (println "Part B:" (transduce (xf (complement first)
+                                    (partial sort (compare-fn ru)))
+                                + xs)))
