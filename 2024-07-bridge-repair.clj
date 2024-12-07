5e8bb8b6586b8167767263de16dec7f9415e3f60
Sam Ferrell <mail@samcf.me>
2024-12-07 04:35:55 -0700

    Optimizations for Day 7, 2024.

--- 2024-07-bridge-repair.clj
+++ 2024-07-bridge-repair.clj
@@ -1,16 +1,20 @@
+(require '[clojure.math :refer [log10 pow]])
+
+(defn join [x y] (long (+ (* x (pow 10 (int (inc (log10 y))))) y)))
+
 (defn solve-a [t x [y & xs]]
-  (if (<= x t)
-    (let [a (+ x y) b (* x y)]
-      (if (seq xs)
-        (or (solve-a t a xs) (solve-a t b xs))
-        (or (= t a) (= t b)))) false))
+  (if (> x t) false
+      (let [a (+ x y) b (* x y)]
+        (if (seq xs)
+          (or (solve-a t a xs) (solve-a t b xs))
+          (or (= t a) (= t b))))))
 
 (defn solve-b [t x [y & xs]]
-  (if (<= x t)
-    (let [a (+ x y) b (* x y) c (parse-long (str x y))]
-      (if (seq xs)
-        (or (solve-b t a xs) (solve-b t b xs) (solve-b t c xs))
-        (or (= t a) (= t b) (= t c)))) false))
+  (if (> x t) false
+      (let [a (+ x y) b (* x y) c (join x y)]
+        (if (seq xs)
+          (or (solve-b t a xs) (solve-b t b xs) (solve-b t c xs))
+          (or (= t a) (= t b) (= t c))))))
 
 (let [in (line-seq (java.io.BufferedReader. *in*))
       xf (fn [f]
