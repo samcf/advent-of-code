ae755b5e443354d63ee9837b7891e4b6b405950c
Sam Ferrell <mail@samcf.me>
2024-12-12 05:10:41 -0500

    Simplify solution for Day 11, 2024.

--- 2024-11-plutonium-pebbles.clj
+++ 2024-11-plutonium-pebbles.clj
@@ -2,21 +2,21 @@
 
 (def stones
   (memoize
-   (fn [x itr max]
-     (if (< itr max)
+   (fn [x n]
+     (if (> n 0)
        (if (= x 0)
-         (recur 1 (inc itr) max)
+         (recur 1 (dec n))
          (let [d (int (inc (log10 x)))]
            (if (even? d)
              (let [div (pow 10 (/ d 2))]
-               (+ (stones (int (quot x div)) (inc itr) max)
-                  (stones (int (mod  x div)) (inc itr) max)))
-             (recur (* x 2024) (inc itr) max)))) 1))))
+               (+ (stones (int (quot x div)) (dec n))
+                  (stones (int (mod  x div)) (dec n))))
+             (recur (* x 2024) (dec n))))) 1))))
 
 (let [in (re-seq #"\d+" (first (line-seq (java.io.BufferedReader. *in*))))
-      xf (fn [max]
+      xf (fn [n]
            (comp (map str)
                  (map parse-long)
-                 (map (fn [x] (stones x 0 max)))))]
+                 (map (fn [x] (stones x n)))))]
   (println "Part A:" (transduce (xf 25) + in))
   (println "Part B:" (transduce (xf 75) + in)))
