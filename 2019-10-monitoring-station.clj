55320daa1dd277f0d39d866b9a8682d7ed15dd55
Sam Ferrell <mail@samcf.me>
2025-09-05 14:16:21 -0600

    Improve solution for Day 10, 2019.

--- 2019-10-monitoring-station.clj
+++ 2019-10-monitoring-station.clj
@@ -8,44 +8,34 @@
    (filter (comp #{\#} second))
    (map first)))
 
-(defn angle [[ax ay] [bx by]]
-  (let [r (Math/atan2 (- bx ax) (- ay by))]
-    (if (< r 0) (+ tau r) r)))
+(defn angle [[ax ay]]
+  (fn [[bx by]]
+    (let [r (Math/atan2 (- bx ax) (- ay by))]
+      (if (< r 0) (+ tau r) r))))
 
-(defn distance [[ax ay] [bx by]]
-  (max (abs (- bx ax)) (abs (- by ay))))
-
-(defn comp-fn [a]
-  (fn [b c]
-    (compare
-     (distance a b)
-     (distance a c))))
+(defn angles [xs a]
+  (count (into (hash-set) (map (angle a)) (disj xs a))))
 
 (defn asteroids [xs a]
-  (let [cmp (comp-fn a)]
-    (reduce
-     (fn [m x] (update m (angle a x) (fnil conj (sorted-set-by cmp)) x))
-     (sorted-map-by <)
-     (disj xs a))))
+  (let [f (angle a)]
+    (apply merge-with into (map (fn [b] {(f b) [b]}) xs))))
 
 (defn vaporized-at [rs n]
-  (loop [rs rs ks (cycle (keys rs)) nth 0 point nil]
+  (loop [rs rs ks (cycle (sort (keys rs))) nth 0 point nil]
     (if (not= nth n)
       (let [xs (rs (first ks))]
         (if (> (count xs) 1)
           (recur (update rs (first ks) rest) (rest ks) (inc nth) (first xs))
-          (recur (dissoc rs (first ks))      (rest ks) (inc nth) (first xs))))
-      point)))
+          (recur (dissoc rs (first ks))      (rest ks) (inc nth) (first xs)))) point)))
 
 (let [in (line-seq (java.io.BufferedReader. *in*))
-      xs (into #{} (parse (count in)) in)
-      rs (into  {} (map (fn [a] [a (asteroids xs a)])) xs)
-      kv (first
+      xs (into (hash-set) (parse (count in)) in)
+      rs (first
           (into
            (sorted-map-by >)
-           (map (juxt (comp count val) key)) rs))]
-  (println "Part A:" (key kv))
+           (map (fn [a] [(angles xs a) a])) xs))]
+  (println "Part A:" (key rs))
   (println
    "Part B:"
-   (let [[x y] (vaporized-at (rs (val kv)) 200)]
+   (let [[x y] (vaporized-at (asteroids xs (val rs)) 200)]
      (+ (* x 100) y))))
