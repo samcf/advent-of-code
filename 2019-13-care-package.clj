bd91e527e36099cafc78a8cc483f8dd6d439fa6f
Sam Ferrell <mail@samcf.me>
2025-09-16 15:09:21 -0600

    Simplify solution for Day 13, 2019.

--- 2019-13-care-package.clj
+++ 2019-13-care-package.clj
@@ -12,27 +12,24 @@
          (let [_ (<! dst) t (<! dst)]
            (recur (if (= t 2) (inc sum) sum))))))))
 
-(defn play [xs]
+(defn score [xs]
   (let [[src dst out] (intcode xs)]
-    (go-loop [rs {} ax 0 bx 0 sum 0]
+    (go-loop [ax 0 bx 0 sum 0]
       (let [sx (long (signum (- ax bx)))]
         (alt!
-          [[src sx]] (recur rs ax bx sum)
+          [[src sx]] (recur ax bx sum)
           out sum
-          dst
-          ([x]
-           (let [y (<! dst) t (<! dst)]
-             (if (and (= x -1) (= y 0))
-               (recur rs ax bx t)
-               (let [rs (assoc rs [x y] t)]
-                 (case t
-                   3 (recur rs ax x sum)
-                   4 (recur rs x bx sum)
-                   (recur rs ax bx sum)))))))))))
+          dst ([x]
+               (let [_ (<! dst) t (<! dst)]
+                 (cond
+                   (= x -1) (recur ax bx t)
+                   (= t  3) (recur ax x sum)
+                   (= t  4) (recur x bx sum)
+                   :else    (recur ax bx sum)))))))))
 
 (let [in (first (line-seq (java.io.BufferedReader. *in*)))
       re (re-seq #"-?\d+" in)
       xf (comp (map parse-long) (map-indexed vector))
       xs (into {} xf re)]
   (println "Part A:" (<!! (blocks xs)))
-  (println "Part B:" (<!! (play (assoc xs 0 2)))))
+  (println "Part B:" (<!! (score (assoc xs 0 2)))))
