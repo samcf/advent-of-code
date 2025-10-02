6cb4dd5f9e6af698d6752e5aa1e4fd9caa3eb69a
Sam Ferrell <mail@samcf.me>
2025-10-02 12:31:43 -0600

    Minor refactor for Day 5, 2018.

--- 2018-05-alchemical-reduction.clj
+++ 2018-05-alchemical-reduction.clj
@@ -5,16 +5,15 @@
        (rest rs)
        (conj rs x))) (list) xs))
 
-(defn process [ch]
-  (comp (map int) (remove #{ch (+ ch 32)})))
-
 (defn experiment [xs]
-  (comp (map (fn [ch] (into [] (process ch) xs)))
+  (comp (map (fn [ch] (remove #{ch (+ ch 32)})))
+        (map (fn [xf] (into [] xf xs)))
         (map react)
         (map count)))
 
-(let [in (first (line-seq (java.io.BufferedReader. *in*)))]
-  (println "Part A:" (count (react (into [] (map int) in))))
+(let [in (first (line-seq (java.io.BufferedReader. *in*)))
+      xs (into [] (map int) in)]
+  (println "Part A:" (count (react xs)))
   (println "Part B:" (transduce
-                      (experiment in) min ##Inf
+                      (experiment xs) min ##Inf
                       (range (int \A) (inc (int \Z))))))
