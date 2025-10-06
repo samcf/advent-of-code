5c8663317a340e1a095733b476d1601b30422657
Sam Ferrell <mail@samcf.me>
2025-10-06 01:41:21 -0600

    Improve solution for Day 6, 2018.

--- 2018-06-chronal-coordinates.clj
+++ 2018-06-chronal-coordinates.clj
@@ -2,37 +2,44 @@
   [{:x -1 :y 0} {:x 0 :y -1} {:x 1 :y 0} {:x 0 :y 1}])
 
 (defn add [a b]
-  {:x (+ (:x a) (:x b)) :y (+ (:y a) (:y b))})
+  {:x (+ (:x a) (:x b))
+   :y (+ (:y a) (:y b))})
 
 (defn div [a n]
-  {:x (quot (:x a) n) :y (quot (:y a) n)})
+  {:x (quot (:x a) n)
+   :y (quot (:y a) n)})
+
+(defn bounds [xs f]
+  (reduce
+   (fn [a b]
+     {:x (f (:x a) (:x b))
+      :y (f (:y a) (:y b))}) xs))
 
 (defn manhattan [a b]
   (+ (abs (- (:x a) (:x b)))
      (abs (- (:y a) (:y b)))))
+
+(defn area [pred boundary? start]
+  (loop [queue (conj clojure.lang.PersistentQueue/EMPTY start)
+         vstd (hash-set)
+         area 0]
+    (if (seq queue)
+      (let [point (peek queue) queue (pop queue)]
+        (cond (vstd point)       (recur queue vstd area)
+              (not (pred point)) (recur queue (conj vstd point) area)
+              (boundary? point)  0
+              :else
+              (recur (into queue (map (fn [uv] (add point uv))) neighbors)
+                     (conj vstd point)
+                     (inc area)))) area)))
 
 (defn nearest [xs a]
   (let [[c d] (sort-by val (into {} (map (fn [b] {b (manhattan a b)})) xs))]
     (if (= (val c) (val d)) nil
         (key c))))
 
-(defn area [pred limit point]
-  (loop [queue (conj clojure.lang.PersistentQueue/EMPTY point)
-         vstd  (hash-set)
-         area  0]
-    (if (> area limit) 0
-        (if (not (seq queue)) area
-            (let [next (peek queue) list (pop queue)]
-              (if (vstd next)
-                (recur list vstd area)
-                (if (not (pred next))
-                  (recur list (conj vstd next) area)
-                  (recur (into list (map (fn [off] (add next off))) neighbors)
-                         (conj vstd next)
-                         (inc area)))))))))
-
-(defn area-nearest [xs n]
-  (map (fn [a] (area (fn [b] (= (nearest xs b) a)) n a))))
+(defn area-nearest [xs f]
+  (map (fn [a] (area (fn [b] (= (nearest xs b) a)) f a))))
 
 (defn area-within [xs n]
   (fn [a] (< (transduce (map (fn [b] (manhattan a b))) + xs) n)))
@@ -41,7 +48,14 @@
       xf (comp (map (fn [ln] (re-seq #"\d+" ln)))
                (map (fn [[x y]] [(parse-long x) (parse-long y)]))
                (map (fn [[x y]] {:x x :y y})))
-      xs (into [] xf in)]
-  (println "Part A:" (transduce (area-nearest xs 5000) max 0 xs))
-  (println "Part B:" (area (area-within xs 10000) ##Inf
+      xs (into [] xf in)
+      ba (bounds xs min)
+      bb (bounds xs max)
+      bf (fn [a]
+           (or (= (:x a) (:x ba))
+               (= (:y a) (:y ba))
+               (= (:x a) (:x bb))
+               (= (:y a) (:y bb))))]
+  (println "Part A:" (transduce (area-nearest xs bf) max 0 xs))
+  (println "Part B:" (area (area-within xs 10000) bf
                            (div (reduce add xs) (count xs)))))
