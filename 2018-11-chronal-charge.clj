43e244b52ea4955df60bb8f260b2f1a0e18150cf
Sam Ferrell <mail@samcf.me>
2025-10-13 03:34:45 -0600

    Fix var names, etc for Day 11, 2018.

--- 2018-11-chronal-charge.clj
+++ 2018-11-chronal-charge.clj
@@ -6,33 +6,33 @@
       (-> (* a y) (+ n) (* a) (quot 100) (mod 10) (- 5)))))
 
 (defn sums [f]
-  (loop [y 1 x 1 s {}]
+  (loop [x 1 y 1 s {}]
     (if (> y 300) s
         (if (> x 300)
-          (recur (inc y) 1 s)
+          (recur 1 (inc y) s)
           (let [p (f x y)
                 a (get s [(dec x) y] 0)
                 b (get s [x (dec y)] 0)
                 c (get s [(dec x) (dec y)] 0)
                 t (- (+ a b p) c)]
-            (recur y (inc x) (assoc s [x y] t)))))))
+            (recur (inc x) y (assoc s [x y] t)))))))
 
 (defn search [s j k]
   (loop [ax 1 ay 1 sz (dec j) max (first {[0 0 0] 0})]
-    (let [bx (+ ay sz) by (+ ax sz)]
+    (let [bx (+ ax sz) by (+ ay sz)]
       (if (= sz k) (key max)
           (if (> by 300)
             (recur 1 1 (inc sz) max)
             (if (> bx 300)
-              (recur (inc ax) 1 sz max)
+              (recur 1 (inc ay) sz max)
               (let [a (get s [bx by] 0)
-                    b (get s [(dec ay) by] 0)
-                    c (get s [bx (dec ax)] 0)
-                    d (get s [(dec ay) (dec ax)] 0)
+                    b (get s [(dec ax) by] 0)
+                    c (get s [bx (dec ay)] 0)
+                    d (get s [(dec ax) (dec ay)] 0)
                     t (+ (- a b c) d)]
                 (if (> t (val max))
-                  (recur ax (inc ay) sz (first {[ay ax (inc sz)] t}))
-                  (recur ax (inc ay) sz max)))))))))
+                  (recur (inc ax) ay sz (first {[ax ay (inc sz)] t}))
+                  (recur (inc ax) ay sz max)))))))))
 
 (let [in (first (line-seq (java.io.BufferedReader. *in*)))
       xs (sums (cell (parse-long in)))]
