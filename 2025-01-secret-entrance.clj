0e6e85d96d712fd8c79493ff3bf1b13a97493465
Sam Ferrell <mail@samcf.me>
2025-11-30 23:51:50 -0700

    Minor changes to Day 1, 2025.

--- 2025-01-secret-entrance.clj
+++ 2025-01-secret-entrance.clj
@@ -2,29 +2,29 @@
 
 (defn parse [ln]
   (condp re-find ln
-    #"R(\d+)" :>> (fn [[_ a]] (parse-long a))
-    #"L(\d+)" :>> (fn [[_ a]] (* (parse-long a) -1))))
+    #"R(\d+)" :>> (fn [[_ a]] (+ (parse-long a)))
+    #"L(\d+)" :>> (fn [[_ a]] (- (parse-long a)))))
 
-(defn between [a b s]
-  (let [x (ceil  (/ (min a b) s))
-        y (floor (/ (max a b) s))]
-    (int (max 0 (inc (- y x))))))
+(defn between [x y m]
+  (let [a (ceil  (/ (min x y) m))
+        b (floor (/ (max x y) m))]
+    (int (max 0 (inc (- b a))))))
 
-(defn stop [_ x]
-  (if (zero? (mod x 100)) 1 0))
+(defn stop [_ y]
+  (if (zero? (mod y 100)) 1 0))
 
-(defn stops [m x]
-  (let [n (between (:pos m) x 100)]
-    (if (zero? (mod (:pos m) 100))
+(defn stops [x y]
+  (let [n (between x y 100)]
+    (if (zero? (mod x 100))
       (max (dec n) 0) n)))
 
 (defn solve [f]
   (fn
     ([] {:pos 50 :sum 0})
     ([m] (:sum m))
-    ([m x]
-     (let [y (+ (:pos m) x)]
-       (update (assoc m :pos y) :sum + (f m y))))))
+    ([m o]
+     (let [x (:pos m) y (+ x o)]
+       (update (assoc m :pos y) :sum + (f x y))))))
 
 (let [in (line-seq (java.io.BufferedReader. *in*))]
   (println "Part A:" (transduce (map parse) (solve stop) in))
