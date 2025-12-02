ea8be7be21d728ded2ac6a66d6bda6e3611181ee
Sam Ferrell <mail@samcf.me>
2025-12-02 00:38:08 -0700

    Improvements to Day 2, 2025.

--- 2025-02-gift-shop.clj
+++ 2025-02-gift-shop.clj
@@ -1,13 +1,24 @@
-(require '[clojure.math :refer [floor log10 pow]])
+(require '[clojure.math :refer [floor log10 pow sqrt]])
+
+(def factors
+  (memoize
+   (fn [n]
+     (sequence
+      (comp
+       (filter (fn [x] (zero? (mod n x))))
+       (mapcat (fn [x] [x (quot n x)]))
+       (distinct)
+       (remove #{n}))
+      (range 1 (long (inc (sqrt n))))))))
 
 (defn digits [x]
-  (int (inc (floor (log10 x)))))
+  (long (inc (floor (log10 x)))))
 
 (defn pattern? [x n]
   (let [base (pow 10 n)]
     (loop [a x b nil]
       (if (zero? a) true
-          (let [c (int (mod a base))]
+          (let [c (long (mod a base))]
             (if (and b (not= b c)) false
                 (recur (quot a base) c)))))))
 
@@ -17,19 +28,15 @@
         (pattern? x (quot d 2)))))
 
 (defn invalid' [x]
-  (let [d (digits x)]
-    (loop [n (dec d)]
-      (if (zero? n) false
-          (if (and (zero? (rem d n)) (pattern? x n)) true
-              (recur (dec n)))))))
+  (some
+   (fn [n] (when (pattern? x n) x))
+   (factors (digits x))))
 
 (let [in (first (line-seq (java.io.BufferedReader. *in*)))
       re (re-seq #"\d+" in)
-      xf (fn [f]
-           (comp
-            (map parse-long)
-            (partition-all 2)
-            (mapcat (fn [[a b]] (range a (inc b))))
-            (keep (fn [x] (when (f x) x)))))]
-  (println "Part A:" (transduce (xf invalid)  + re))
-  (println "Part B:" (transduce (xf invalid') + re)))
+      xf (comp
+          (map parse-long)
+          (partition-all 2)
+          (mapcat (fn [[a b]] (range a (inc b)))))]
+  (println "Part A:" (transduce (comp xf (filter invalid))  + re))
+  (println "Part B:" (transduce (comp xf (filter invalid')) + re)))
