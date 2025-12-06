470af259b3c266e718167488ac6d452a3de3fcf5
Sam Ferrell <mail@samcf.me>
2025-12-05 22:56:47 -0700

    Improved solution for Day 5, 2025.

--- 2025-05-cafeteria.clj
+++ 2025-05-cafeteria.clj
@@ -1,40 +1,21 @@
 (defn parse
-  ([] {:ranges (hash-set) :ids []})
-  ([m ln]
+  ([] {:ranges (sorted-set) :ids []})
+  ([rs ln]
    (condp re-find ln
-     #"(\d+)-(\d+)" :>> (fn [[_ a b]] (update m :ranges conj [(parse-long a) (parse-long b)]))
-     #"(\d+)"       :>> (fn [[_   a]] (update m :ids    conj (parse-long a)))
-     m)))
+     #"(\d+)-(\d+)" :>> (fn [[_ a b]] (update rs :ranges conj [(parse-long a) (parse-long b)]))
+     #"(\d+)"       :>> (fn [[_   a]] (update rs :ids    conj (parse-long a)))
+     rs)))
 
-(defn fresh? [rs id]
-  (some (fn [[a b]] (and (>= id a) (<= id b))) rs))
+(defn fresh [xs id]
+  (some (fn [[a b]] (and (>= id a) (<= id b))) xs))
 
-(defn overlap? [[a b]]
-  (fn [[c d]]
-    (<= (max a c) (min b d))))
-
-(defn subset? [[a b]]
-  (fn [[c d]]
-    (and (>= c a) (<= d b))))
-
-(defn split [[a b] [c d]]
-  (let [mid (max a c)]
-    [[(min a c) mid]
-     [(inc mid) (max b d)]]))
-
-(defn discrete [rs]
-  (loop [rs rs sum 0]
-    (if-let [a (first rs)]
-      (let [rest (disj rs a)]
-        (if-let [b (first (filter (subset? a) rest))]
-          (recur (disj rs b) sum)
-          (if-let [b (first (filter (overlap? a) rest))]
-            (recur (into (disj rs a b) (split a b)) sum)
-            (recur rest (+ sum (inc (- (a 1) (a 0))))))))
-      sum)))
+(defn disjoint [[[a b] :as xs] [c d :as x]]
+  (if (<= (max a c) (min b d))
+    (conj (rest xs) [(min a c) (max b d)])
+    (conj xs x)))
 
 (let [in (line-seq (java.io.BufferedReader. *in*))
       rs (reduce parse (parse) in)
-      xf (keep (fn [id] (when (fresh? (:ranges rs) id) 1)))]
-  (println "Part A:" (transduce xf + (:ids rs)))
-  (println "Part B:" (discrete (:ranges rs))))
+      xs (reduce disjoint (list (first (:ranges rs))) (:ranges rs))]
+  (println "Part A:" (transduce (keep (fn [id] (when (fresh xs id) 1))) + (:ids rs)))
+  (println "Part B:" (transduce (map (fn [[a b]] (inc (- b a)))) + xs)))
