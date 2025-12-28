8ebb946d088b6de10a6b1516e35820110a78aff7
Sam Ferrell <mail@samcf.me>
2025-12-27 19:38:03 -0700

    Minor change to Day 9, 2016.

--- 2016-09-explosives-cyberspace.clj
+++ 2016-09-explosives-cyberspace.clj
@@ -7,16 +7,16 @@
         (let [val (xs idx)]
           (cond
             (= val \() (recur (inc idx) 0 mul sum)
-            (= val \)) (recur (+ idx (inc len)) nil nil (+ (* (sum-fn xs idx len) mul) sum))
+            (= val \)) (recur (+ idx (inc len)) nil nil (+ (* (sum-fn xs (inc idx) len) mul) sum))
             (= val \x) (recur (inc idx) len 0 sum)
             mul        (recur (inc idx) len (join mul val) sum)
             len        (recur (inc idx) (join len val) mul sum)
             :else      (recur (inc idx) len mul (inc sum)))))))
 
 (defn recursive [xs idx len]
-  (decompress xs (inc idx) (+ (inc idx) len) recursive))
+  (decompress xs idx (+ idx len) recursive))
 
 (let [in (line-seq (java.io.BufferedReader. *in*))
-      xs (into [] (seq (first in)))]
+      xs (into [] (first in))]
   (println "Part A:" (decompress xs 0 (count xs) (fn [_ _ len] len)))
   (println "Part B:" (decompress xs 0 (count xs) recursive)))
