246145f9c04c855a3b979aa4b996c75d451a52c2
Sam Ferrell <mail@samcf.me>
2026-01-12 11:03:50 -0700

    Minor changes to Day 18.

--- 2016-18-like-a-rogue.clj
+++ 2016-18-like-a-rogue.clj
@@ -5,12 +5,12 @@
       (persistent! rs))))
 
 (defn solve [xs n]
-  (loop [xs xs s 0 t 0]
+  (loop [xs xs sum 0 t 0]
     (if (< t n)
-      (recur (advance xs) (reduce + s xs) (inc t))
-      s)))
+      (recur (advance xs) (reduce + sum xs) (inc t))
+      sum)))
 
 (let [in (first (line-seq (java.io.BufferedReader. *in*)))
       xs (into [] (map (fn [c] (if (= c \.) 1 0))) in)]
-  (time (println "Part A:" (solve xs 40)))
-  (time (println "Part B:" (solve xs 400000))))
+  (println "Part A:" (solve xs 40))
+  (println "Part B:" (solve xs 400000)))
