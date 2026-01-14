49a92967dfd00a85968ffa0f4de487f861f48937
Sam Ferrell <mail@samcf.me>
2026-01-14 10:17:31 -0700

    Inline solution for Day 19, 2016.

--- 2016-19-elephant-named-joseph.clj
+++ 2016-19-elephant-named-joseph.clj
@@ -3,13 +3,7 @@
 (defn nearest [x b]
   (long (pow b (quot (log x) (log b)))))
 
-(defn solve [x]
-  (inc (* (- x (nearest x 2)) 2)))
-
-(defn solve' [x]
-  (inc (- x (inc (nearest x 3)))))
-
 (let [in (line-seq (java.io.BufferedReader. *in*))
       x  (parse-long (first in))]
-  (println "Part A:" (solve  x))
-  (println "Part B:" (solve' x)))
+  (println "Part A:" (inc (* 2 (- x (nearest x 2)))))
+  (println "Part B:" (inc (- x (inc (nearest x 3))))))
